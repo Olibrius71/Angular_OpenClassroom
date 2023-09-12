203b88b021f6f53c3f07dd8fda6307eb75469b12
Anis BEN JEMIA <anis.ben-jemia@etu.univ-lyon1.fr>
2023-09-12 14:08:09 +0200

    Utilisation des opérateurs de bas et haut niveau sur les observables et implémentation des stratégies de unsubscribe()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Snap} from "./models/snap.model";
-import {interval, Observable} from "rxjs";
+import {concatMap, delay, filter, interval, map, mergeMap, Observable, of, switchMap, take, tap} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -9,18 +9,37 @@ import {interval, Observable} from "rxjs";
 })
 export class AppComponent implements OnInit{
 
-  intervalToWatch$!: Observable<number>;
+  interval$!: Observable<number>;
 
-  ngOnInit(): void {
-    this.intervalToWatch$ = interval(1000);
+  ngOnInit() {
 
-    this.intervalToWatch$.subscribe(value => {
-      console.log(value);
-    });
+    this.interval$ = interval(2800);
 
-    setTimeout(() => { this.intervalToWatch$.subscribe(value => {
-      console.log(value);
-    })}, 3000);
+
+    /*
+    interval(500).pipe(
+      take(10),
+      map(value => value % 2 === 0 ? 'rouge' : 'jaune'),
+      tap(color => console.log(`La lumière s'allume en %c${color}`, `color: ${this.translateColor(color)}`)),
+      switchMap(color => this.getTrainObservable$(color)),
+      tap(train => console.log(`Train %c${train.color} ${train.trainIndex} arrivé !`, `font-weight: bold; color: ${this.translateColor(train.color)}`))
+    ).subscribe();
   }
 
+  getTrainObservable$(color: 'rouge' | 'jaune') {
+    const isRedTrain = color === 'rouge';
+    isRedTrain ? this.redTrainsCalled++ : this.yellowTrainsCalled++;
+    const trainIndex = isRedTrain ? this.redTrainsCalled : this.yellowTrainsCalled;
+    console.log(`Train %c${color} ${trainIndex} appelé !`, `text-decoration: underline; color: ${this.translateColor(color)}`);
+    return of({ color, trainIndex }).pipe(
+      delay(isRedTrain ? 5000 : 6000)
+    );
+  }
+
+  translateColor(color: 'rouge' | 'jaune') {
+    return color === 'rouge' ? 'red' : 'yellow';
+  }
+     */
+
+  }
 }
--- src/app/first-comp-list/first-comp-list.component.ts
+++ src/app/first-comp-list/first-comp-list.component.ts
@@ -1,19 +1,34 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Snap} from "../models/snap.model";
 import {SnapService} from "../services/snap.service";
+import {interval, Subject, takeUntil, tap} from "rxjs";
 
 @Component({
   selector: 'app-first-comp-list',
   templateUrl: './first-comp-list.component.html',
   styleUrls: ['./first-comp-list.component.less']
 })
-export class FirstCompListComponent implements OnInit {
+export class FirstCompListComponent implements OnInit, OnDestroy {
   snapList!: Snap[];
+  private destroyed$!: Subject<boolean>;
 
   constructor(private snapService: SnapService) {}
 
   ngOnInit(): void {
     this.snapList = this.snapService.getAllSnaps();
+    this.destroyed$ = new Subject<boolean>();
+
+    interval(560)
+      .pipe(
+        tap(console.log),
+        takeUntil(this.destroyed$)
+        )
+      .subscribe()
+    ;
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed$.next(true);
   }
 
 
