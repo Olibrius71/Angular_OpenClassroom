6ca15887187e419c34ee5c46eabdadc1fe57a917
Anis BEN JEMIA <anis.ben-jemia@etu.univ-lyon1.fr>
2023-09-12 11:36:46 +0200

    Début du 2ème cours, notion d'observables apprise avec interval() et subscribe()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,26 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Snap} from "./models/snap.model";
+import {interval, Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent{
+export class AppComponent implements OnInit{
+
+  intervalToWatch$!: Observable<number>;
+
+  ngOnInit(): void {
+    this.intervalToWatch$ = interval(1000);
+
+    this.intervalToWatch$.subscribe(value => {
+      console.log(value);
+    });
+
+    setTimeout(() => { this.intervalToWatch$.subscribe(value => {
+      console.log(value);
+    })}, 3000);
+  }
 
 }
